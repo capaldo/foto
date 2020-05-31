0482582df9f1eda04677537958b2437a84d2da03
capaldo <alex@capaldo.io>
2020-05-31 01:32:37 -0400

    add updated package file

--- Package.swift
+++ Package.swift
@@ -1,20 +1,18 @@
-// https://www.avanderlee.com/swift/command-line-tool-package-manager/
+// swift-tools-version:5.2
 
-
+/// https://www.avanderlee.com/swift/command-line-tool-package-manager/
 import PackageDescription
 
 let package = Package(
     name: "foto",
+    products: [
+        .executable(name: "foto", targets: ["foto"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
-        .target(
-            name: "foto",
-            dependencies: []),
-        .testTarget(
-            name: "fotoTests",
-            dependencies: ["foto"]),
+        .target(name: "foto", dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
+        .testTarget(name: "fotoTests", dependencies: ["foto"]),
     ]
 )
