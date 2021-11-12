071d860626af79711297fbe07009f6b8d500322a
capaldo <alex@capaldo.io>
2021-11-12 17:22:16 -0500

    Fix version number

--- Sources/foto/main.swift
+++ Sources/foto/main.swift
@@ -4,7 +4,7 @@ import ArgumentParser
 struct Foto: ParsableCommand {
     static let configuration: CommandConfiguration = .init(
         abstract: "A utility to copy, paste, convert, and resize images from the command-line",
-        version: "0.3.0",
+        version: "0.2.1",
         subcommands: [Copy.self, Paste.self]
     )
 }
